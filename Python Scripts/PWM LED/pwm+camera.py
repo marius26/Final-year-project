23fdb3e575d42b26cd4e714a36b6176adc5bc79c
homorogan.marius@yahoo.com <homorogan.marius@yahoo.com>
2024-03-15 09:49:09 +0000

    PWM + shutter speed adjustments

--- Python Scripts/PWM LED/pwm+camera.py
+++ Python Scripts/PWM LED/pwm+camera.py
@@ -16,11 +16,12 @@ def run_libcamera_command():
         "--segment", "1",
         "--width","640",
         "--height","480",
-        "-o", "/home/pi/Desktop/Final-year-project/Python Scripts/PWM LED/640x480-%03d.jpg",
-        "--brightness", "0.0",
-        "--contrast", "0",
+        "-o", "/home/pi/Desktop/Final-year-project/Python Scripts/PWM LED//Photos/640x480-%03d.jpg",
+        "--brightness", "0.1",
+        "--contrast", "0.5",
         "--framerate", "60",
-        "--gain", "0",
+        "--shutter","800",
+        "--gain", "0.5",
         "--awb", "auto",
         "--metering", "centre",
         "--saturation", "1.0",
@@ -44,14 +45,16 @@ def run_pwm():
     ledpin = 35  # PWM pin connected to LED
     GPIO.setmode(GPIO.BOARD)  # set pin numbering system
     GPIO.setup(ledpin, GPIO.OUT)
-    pi_pwm = GPIO.PWM(ledpin, 192000000)  # create PWM instance with frequency
+    pi_pwm = GPIO.PWM(ledpin, 100)  # create PWM instance with frequency
     pi_pwm.start(0)  # start PWM of required Duty Cycle 
 
     while True:
         pi_pwm.ChangeDutyCycle(100)
-        sleep(0.08)
+        sleep(8/1e6)
+        #sleep(1)
         pi_pwm.ChangeDutyCycle(0)
-        sleep(0.08)
+        sleep(6/1e6)
+        
 
 if __name__ == "__main__":
     # Initialize GPIO
@@ -61,13 +64,15 @@ if __name__ == "__main__":
     # Create threads for each script and start them
     libcamera_thread = threading.Thread(target=run_libcamera_command)
     pwm_thread = threading.Thread(target=run_pwm)
-
-    libcamera_thread.start()
+    
     pwm_thread.start()
+    libcamera_thread.start()
+    
 
     # Wait for both threads to finish
-    libcamera_thread.join()
     pwm_thread.join()
+    libcamera_thread.join()
+    
 
     # Cleanup GPIO
     GPIO.cleanup()
